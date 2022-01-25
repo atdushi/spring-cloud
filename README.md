5b4d0ef93d0f9ec7f44937fb8990ff53491ad9f0
atdushi <atdushi@fakemail.com>
2022-01-25 16:48:37 +0300

    project added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# spring-cloud
+- Eureka http://localhost:8761/
+- Client http://localhost:9872/greeting
+- Feign http://localhost:8190/get-greeting
+- Zuul http://localhost:5555/cloud/gclient/greeting
+
+docker-compose up --build -d
+
+docker-compose down
+
---
+++ eureka-client/src/main/java/com/flamexander/cloud/client/GreetingControllerImpl.java
@@ -0,0 +1,39 @@
+package com.flamexander.cloud.client;
+
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingControllerImpl implements GreetingController {
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${userValue}")
+    private String username;
+
+    @Override
+    public String greeting() {
+        return String.format("Hello from '%s'!", eurekaClient.getApplication(appName).getName());
+    }
+
+    @GetMapping("/abc")
+    public String test() {
+        return username;
+//        return "abc";
+    }
+
+
+    @GetMapping("/abcr")
+    public String abcr() {
+        return "ABC";
+    }
+}
